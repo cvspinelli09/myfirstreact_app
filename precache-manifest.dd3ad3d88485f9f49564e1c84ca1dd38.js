3f8e2667c83e76da7044984ec3ad758886867429
cvspinelli09 <53975729+cvspinelli09@users.noreply.github.com>
2019-10-30 00:35:47 +0000

    Updates

---
+++ precache-manifest.dd3ad3d88485f9f49564e1c84ca1dd38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd89ede4e3be406f6f780c72dc38255",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "993f8db667cc9a541627",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "de8375307a71b2f242d7",
+    "url": "/robofriends/static/css/main.6027b67b.chunk.css"
+  },
+  {
+    "revision": "993f8db667cc9a541627",
+    "url": "/robofriends/static/js/2.9e27fb7a.chunk.js"
+  },
+  {
+    "revision": "de8375307a71b2f242d7",
+    "url": "/robofriends/static/js/main.2a381f9c.chunk.js"
+  },
+  {
+    "revision": "5d6eab3efaa93b901743",
+    "url": "/robofriends/static/js/runtime-main.9773c421.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
